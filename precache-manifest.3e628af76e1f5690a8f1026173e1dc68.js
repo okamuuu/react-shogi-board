27d3f530a17b24c917d8cb3413e9138fcfcd9f75
okamuuu <okamuuu@gmail.com>
2019-08-26 23:58:25 +0900

    Updates

---
+++ precache-manifest.3e628af76e1f5690a8f1026173e1dc68.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3c88bacdd1aca2e4e537e51cefaf2a",
+    "url": "/react-shogi-board/index.html"
+  },
+  {
+    "revision": "18e2cb0297cc547e9e84",
+    "url": "/react-shogi-board/static/js/2.2a37c918.chunk.js"
+  },
+  {
+    "revision": "02a1b28df54bc89f269e",
+    "url": "/react-shogi-board/static/js/main.a74850bc.chunk.js"
+  },
+  {
+    "revision": "47f10825d157e8591972",
+    "url": "/react-shogi-board/static/js/runtime~main.93faeb87.js"
+  }
+]);
